74ff00f255457a27c79c9bb0a6f523f2b080aff5
Kevin Gu <kevingu2@illinois.edu>
2022-01-24 09:16:32 -0600

    Update plot info content

--- src/components/atoms/AtomBoxPlotInfo.vue
+++ src/components/atoms/AtomBoxPlotInfo.vue
@@ -2,7 +2,11 @@
   <div class="w-3/6 px-12" >
       <div class="h-full bg-corange rounded-md p-4">
           <h1 class="text-cdark text-2xl pb-2">Box Plot Results</h1>
-          <p class="text-cfontgrey">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet similique sed, cum molestiae culpa nemo quod rerum quia aliquam fugiat?</p>
+          <p class="text-cfontgrey">Sustainability indicators are presented with
+            median values (dark, solid lines), 25th to 75th
+            percentiles (shaded region), 5th and 95th
+            percentiles (lines), and values below and
+            above the 5th and 95th percentiles (points).</p>
       </div>
   </div>
 </template>
--- src/components/atoms/AtomSpearmanInfo.vue
+++ src/components/atoms/AtomSpearmanInfo.vue
@@ -2,7 +2,13 @@
   <div class="w-5/6 py-4">
       <div class="bg-corange rounded-md p-4">
           <h1 class="text-2xl text-cdark pb-2">Spearman's Rank Correlation Outcomes</h1>
-          <p class="text-cfontgrey">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque ipsa adipisci modi perferendis nobis similique quidem repellendus culpa dignissimos quas.</p>
+          <p class="text-cfontgrey">The sensitivity of the indicator to each input
+            parameter is characterized by Spearman’s
+            rank-order correlation, a measure of
+            monotonicity between variables (closer to 1
+            indicates strong positive correlation and
+            closer to -1 indicating strong negative
+            correlation)</p>
       </div>
   </div>
 </template>
