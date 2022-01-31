78506675a282fb44ba66f4e3cb565171865e5752
Kevin Gu <kevingu2@illinois.edu>
2022-01-30 18:22:19 -0600

    Pubs page tweaks

--- src/components/molecules/MoleculePubsCard.vue
+++ src/components/molecules/MoleculePubsCard.vue
@@ -22,7 +22,7 @@
 <!--  </div>-->
   <div class="bg-gray-100 bg-opacity-80 shadow-lg rounded-md ">
     <div class="relative z-10  bg-gray-400 flex flex-col shadow-md" >
-      <h1 class="text-white text-4xl p-5" style="padding-bottom: 1em"><slot name="title">Card Title</slot></h1>
+      <h1 class="text-white text-3xl p-5" style="padding-bottom: 1em"><slot name="title">Card Title</slot></h1>
 <!--      <div class="w-full flex justify-end absolute inset-x-0 py-2 px-6">-->
 <!--        <img class="shadow-xl rounded-full bg-gray-100 border-4 border-gray-400 h-20 w-20 p-1" :src="require('@/assets/resources/' + logo + '')" alt="card logo">-->
 <!--      </div>-->
