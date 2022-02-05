5d455fffe31304c4643c095eec54618cf2e1e2d3
Kevin Gu <kevingu2@illinois.edu>
2022-02-04 19:02:29 -0600

    Box plot info update

--- src/components/atoms/AtomBoxPlotInfo.vue
+++ src/components/atoms/AtomBoxPlotInfo.vue
@@ -3,7 +3,7 @@
     <div class="h-full bg-corange rounded-md p-4">
       <h1 class="text-cdark text-2xl pb-2">Box Plot Results</h1>
       <p class="text-cfontgrey">Sustainability indicators are presented with median values (solid line in the box),
-        25th and 75th percentiles (box edges), and 5th and 95th percentiles (whiskers). Values beyond 5th and 95th percentiles are represented by points.</p>
+        25th and 75th percentiles (box edges), and 5th and 95th percentiles (whiskers). Outliers are represented by points.</p>
     </div>
   </div>
 </template>
